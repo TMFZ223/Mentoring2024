3f7361269731ba7a47ac15124948cdd3a6ab5f56
Сергей Зенин <sergey.zenin@kaspersky.com>
2024-11-10 11:10:43 +0300

    feat: init recipes service

---
+++ recipes-sevice/src/db/init.js
@@ -0,0 +1,55 @@
+const { Sequelize, DataTypes, Model } = require('sequelize');
+
+module.exports.initDbConnection = async () => {
+  const sequelize = new Sequelize({
+    dialect: 'postgres',
+    host: 'host.docker.internal',
+    port: 5432,
+    username: process.env.POSTGRES_USER,
+    password: process.env.POSTGRES_PASSWORD,
+    database: 'postgres_db'
+  })
+
+  try {
+    await sequelize.authenticate();
+    console.log('Connection has been established successfully.');
+  } catch (error) {
+    console.error('Unable to connect to the database:', error);
+  }
+
+  class RecipesModel extends Model {}
+
+  RecipesModel.init({
+    id: {
+      type: DataTypes.UUID,
+      primaryKey: true,
+      defaultValue: DataTypes.UUIDV4
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    description: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    ingredients: {
+      type: DataTypes.ARRAY(DataTypes.STRING),
+      allowNull: false
+    },
+    instructions: {
+      type: DataTypes.ARRAY(DataTypes.STRING),
+      allowNull: false
+    },
+    image: {
+      type: DataTypes.STRING,
+      allowNull: true
+    },
+  }, 
+  {sequelize}
+);
+
+  await RecipesModel.sync();
+
+  return { RecipesModel };
+};
---
+++ recipes-sevice/src/web-server.js
@@ -0,0 +1,36 @@
+const { initDbConnection } = require('./db/init.js');
+const express = require('express');
+
+const app = express();
+app.use(express.json());
+
+(async () => {
+  const { RecipesModel } = await initDbConnection();
+
+  app.post("/recipes", async (req, res) => {
+    try {
+      const recipe = await RecipesModel.create(req.body);
+      res.json(recipe);
+    } catch (err) {
+      console.log(err);
+
+      if (err?.name === 'SequelizeValidationError') {
+        const errors = err.errors.map(({ message, type, path, value }) => ({ message, type, path, value }));
+        res.status(400).json({ errors });
+        return
+      }
+
+      res.status(500).json(err);
+    }
+  });
+
+  app.get("/recipes", async (req, res) => {
+    const { page = 0 } = req.query;
+    const recipes = await RecipesModel.findAndCountAll({ limit: 10, offset: page * 10 });
+    res.json(recipes);
+  });
+
+  app.listen(7000, () => {
+    console.log("Server is running on port 7000");
+  });
+})();
